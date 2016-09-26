cd33141dde793f8513f6bad2b6ddb37b4d2ad6fc
Brandon Weaver <keystonelemur@gmail.com>
2016-09-26 08:37:06 -0700

    forgot baseurl

--- gulp/tasks/production.js
+++ gulp/tasks/production.js
@@ -1,12 +1,22 @@
 'use strict';
 
 import gulp        from 'gulp';
+import config      from '../config';
 import runSequence from 'run-sequence';
+import htmlReplace from 'gulp-html-replace';
+
+gulp.task('replaceBaseUrl', function () {
+  return gulp.src(config.views.index)
+    .pipe(htmlReplace({
+      baseUrl: `<base href="${config.baseUrl}">`
+    }))
+    .pipe(gulp.dest(config.buildDir));
+});
 
 gulp.task('prod', ['clean'], function(cb) {
   cb = cb || function() {};
 
   global.isProd = true;
 
-  runSequence(['styles', 'images', 'fonts', 'views'], 'browserify', 'gzip', cb);
+  runSequence(['styles', 'images', 'fonts', 'views', 'replaceBaseUrl'], 'browserify', 'gzip', cb);
 });
